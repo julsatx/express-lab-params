e59a513c4a36ad5cbc3288ec370462c7fa461c32
Julius Camarillo <julcamarillo17@gmail.com>
2019-01-15 15:44:45 -0600

    tip calculator added

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 let filestream = require('fs')
 let express = require('express')
 let app = express()
+let tipCalc = function(total,tipPercent){
+	return total *(tipPercent/100)
+}
 
-app.get('/greeting/:name', (request, response) => {
-  response.send(`Helloo ${request.params.name}`)
-});
+// app.get('/greeting/:name', (request, response) => {
+//   response.send(`Helloo ${request.params.name}`)
+// });
+
+
+
+app.get('/tip/:total/:tipPercent',(request,response)=>{
+	response.send(`Your tip is ${tipCalc(request.params.total,request.params.tipPercent)}`)
+})
 
 app.listen(9000)
